fc75d95ace8e44348c34a49c74c445a3668629d9
dawei_hz <cwwjsyc@126.com>
2021-03-07 23:30:48 +0800

    Update Time.php

--- Language/zh-CN/Time.php
+++ Language/zh-CN/Time.php
@@ -1,15 +1,3 @@
-<?php
-
-/**
- * This file is part of the CodeIgniter 4 framework.
- *
- * (c) CodeIgniter Foundation <admin@codeigniter.com>
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- */
-
-// Time language settings
 return [
 	'invalidMonth'   => '月份必须在 1 至 12 之间。 现在是: {0}',
 	'invalidDay'     => '天数必须在 1 至 31 之间。 现在是: {0}',
@@ -17,6 +5,13 @@ return [
 	'invalidHours'   => '小时必须在 0 至 23 之间。 现在是: {0}',
 	'invalidMinutes' => '分钟必须在 0 至 59 之间。 现在是: {0}',
 	'invalidSeconds' => '秒数必须在 0 至 59 之间。 现在是: {0}',
+	'years'          => '{0, plural, =1{# 年} other{# 年}}',
+	'months'         => '{0, plural, =1{# 月} other{# 月}}',
+	'weeks'          => '{0, plural, =1{# 周} other{# 周}}',
+	'days'           => '{0, plural, =1{# 天} other{# 天}}',
+	'hours'          => '{0, plural, =1{# 小时} other{# 小时}}',
+	'minutes'        => '{0, plural, =1{# 分钟} other{# 分钟}}',
+	'seconds'        => '{0, plural, =1{# 秒} other{# 秒}}',
 	'ago'            => '{0} 以前',
 	'inFuture'       => '在 {0}',
 	'yesterday'      => '昨天',
